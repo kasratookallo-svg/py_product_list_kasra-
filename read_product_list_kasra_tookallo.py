3f8ba85ea19632d63ef716dd12f7b3e1a7c9fe57
Jalase10_git_kasra <kasratookallo@gmail.com>
2025-10-20 14:49:34 +0330

    show data is updated.

--- read_product_list_kasra_tookallo.py
+++ read_product_list_kasra_tookallo.py
@@ -5,7 +5,4 @@ file = open("product_list_Kasra.dat", "rb")
 product_list = pickle.load(file)
 file.close()
 print(product_list)
-for product in product_list:
-    print(
-        f"{product['name']:10}  -  {product['brand']:10} - ( Tedad :{product['quantity']:>3}) (Price :{product['price']:>8})")
-
+show_data(product_list)
